594bfae445b7b9251ff8ee41348574a0dec6be7a
muahahammm <aahh853853@gmail.com>
2026-02-07 21:09:16 +0700

    Xây dựng trang web nhóm quyền

---
+++ models/role.model.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const roleSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    permision: { type: Array, default: [] },
+    deleted: { type: Boolean, default: false },
+    deleteAt: Date
+}, { timestamps: true });
+
+const Role = mongoose.model('Role', roleSchema, "roles");
+module.exports = Role; 
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -3,6 +3,7 @@ const systemConfig = require("../../config/system");
 const dashboardRoute = require("./dashboard.route");
 const productRoute = require("./product.route");
 const productCategoryRoute = require("./product-category.route");
+const roleRoute = require("./role.route");
 
 module.exports = (app) => {
     const PATH_ADMIN = systemConfig.prefixAdmin;
@@ -12,4 +13,6 @@ module.exports = (app) => {
     app.use(PATH_ADMIN + "/products", productRoute);
 
     app.use(PATH_ADMIN + "/products-category", productCategoryRoute);
+
+    app.use(PATH_ADMIN + "/roles", roleRoute);
 };
