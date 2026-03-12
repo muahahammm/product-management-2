4f2d51577e36454701ad45aa62e7ea54bce451cc
muahahammm <aahh853853@gmail.com>
2026-03-12 21:01:59 +0700

    Lấy ra sản phẩm mới nhất

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -11,11 +11,21 @@ module.exports.index = async (req, res) => {
         status: "active"
     }).limit(1);
 
-    const newProducts = productsHelper.priceNewProducts(productsFeature);
+    const newProductsFeature = productsHelper.priceNewProducts(productsFeature);
     // Kết thúc lấy ra sản phẩm nổi bật
+
+    // Lấy ra sản phẩm mới nhất
+    const productsNew = await Product.find({
+        deleted: false,
+        status: "active"
+    }).sort({ position : "desc" }).limit(6);
+
+    const newProducts = productsHelper.priceNewProducts(productsNew);
+    // Kết thúc lấy ra sản phẩm mới nhất
 
     res.render("client/pages/home/index.pug", {
         pageTitle: "Trang chủ",
-        productsFeature: newProducts
+        productsFeature: newProductsFeature,
+        productsNew: newProducts
     });
 }
