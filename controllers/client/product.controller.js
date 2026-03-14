56a2d3809da70c7d6e7992675ff721dca0cde06d
muahahammm <aahh853853@gmail.com>
2026-03-14 11:20:35 +0700

    Làm trang chi tiết sản phẩm

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -20,23 +20,35 @@ module.exports.index = async (req, res) => {
 }
 
 
-// [GET] /products/detail
+// [GET] /products/detail/:slugProduct
 module.exports.detail = async (req, res) => {
     try {
         const find = {
             deleted: false, 
-            slug: req.params.slug,
+            slug: req.params.slugProduct,
             status: "active"
         }
 
         const product = await Product.findOne(find);
+
+        if(product.product_category_id) {
+            const category = await ProductCategory.findOne({
+                _id: product.product_category_id,
+                status: "active",
+                deleted: false
+            });
+
+            product.category = category;
+        }
+
+        product.priceNew = productsHelper.priceNewProduct(product);
 
         res.render("client/pages/products/detail.pug", {
             pageTitle: product.title,
             product: product
         });
     } catch (error) {
-        
+        res.redirect(req.params.header);
     }
 }
 
