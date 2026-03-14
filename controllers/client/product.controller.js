4b1f8ebfd13a50a8475bbb9117fb4bb56828d80d
muahahammm <aahh853853@gmail.com>
2026-03-14 10:01:01 +0700

    Lấy danh sách sản phẩm bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,6 +1,8 @@
 const Product = require("../../models/product.model.js");
+const ProductCategory = require("../../models/product-category.model.js");
 
 const productsHelper = require("../../helpers/products");
+const productCategoryHelper = require("../../helpers/product-category.js");
 
 // [GET] /products
 module.exports.index = async (req, res) => {
@@ -36,4 +38,29 @@ module.exports.detail = async (req, res) => {
     } catch (error) {
         
     }
+}
+
+
+// [GET] /products/:slugCategory
+module.exports.category = async (req, res) => {
+    const category = await ProductCategory.findOne({
+        slug: req.params.slugCategory, 
+        deleted: false,
+        status: "active"
+    });
+
+    const listSubCategory = await productCategoryHelper.getSubCategory(category.id);
+    const listSubCategoryId = listSubCategory.map(item => item.id);
+    
+    const products = await Product.find({
+        product_category_id: { $in : [category.id, ...listSubCategoryId ]},
+        deleted: false
+    }).sort({ position : "desc" });
+    
+    const newProducts = productsHelper.priceNewProducts(products);
+
+    res.render("client/pages/products/index.pug", {
+        pageTitle: category.title,
+        products: newProducts
+    });
 }
