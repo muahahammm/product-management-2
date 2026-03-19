b48e4c43c93434b40c75dfb3a70c2be7519aabfa
muahahammm <aahh853853@gmail.com>
2026-03-19 17:51:41 +0700

    Tính tổng lượng sản phẩm

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -10,6 +10,11 @@ module.exports.cartId = async (req, res, next) => {
         res.cookie("cartId", cart.id, { expires: new Date(Date.now() + expireTime) });
     } else {
         // Lấy giỏ hàng
+        const cart = await Cart.findOne({
+            _id: req.cookies.cartId
+        });
+        cart.totalQuantity = cart.products.reduce((sum, item) => sum + item.quantity, 0);
+        res.locals.miniCart = cart;
     }
 
     next();
