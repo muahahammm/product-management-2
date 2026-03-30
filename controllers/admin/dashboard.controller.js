1a6d248bc4e08d54270896a0e3a53eeeef233062
muahahammm <aahh853853@gmail.com>
2026-03-30 21:00:50 +0700

    Trang dashboard admin

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,6 +1,72 @@
+const ProductCategory = require("../../models/product-category.model");
+const Product = require("../../models/product.model");
+const Account = require("../../models/account.model");
+const User = require("../../models/user.model");
+const ArticleCategory = require("../../models/article-category.model");
+const Article = require("../../models/article.model");
+
+
 // [get] /admin/dashboard
-module.exports.dashboard = (req, res) => {
+module.exports.dashboard = async (req, res) => {
+    const statistic = {
+        categoryProduct: {
+            total: 0,
+            active: 0,
+            inactive: 0
+        },
+        product: {
+            total: 0,
+            active: 0,
+            inactive: 0
+        },
+        account: {
+            total: 0,
+            active: 0,
+            inactive: 0
+        },
+        user: {
+            total: 0,
+            active: 0,
+            inactive: 0
+        },
+        categoryArticle: {
+            total: 0,
+            active: 0,
+            inactive: 0
+        },
+        article: {
+            total: 0,
+            active: 0,
+            inactive: 0
+        },
+    }
+
+    const countRecordModelAll = async (Model) => {
+        const [total, active, inactive] = await Promise.all([
+            Model.countDocuments({
+                deleted: false
+            }),
+            Model.countDocuments({
+                deleted: false,
+                status: "active"
+            }),
+            Model.countDocuments({
+                deleted: false,
+                status: "inactive"
+            })
+        ]);
+
+        return { total, active, inactive };
+    }
+
+    statistic.categoryProduct = await countRecordModelAll(ProductCategory);
+    statistic.product = await countRecordModelAll(Product);
+    statistic.account = await countRecordModelAll(Account);
+    statistic.user = await countRecordModelAll(User);
+    statistic.categoryArticle = await countRecordModelAll(ArticleCategory); 
+
     res.render("admin/pages/dashboard/index", {
-        pageTitle: "Trang tổng quan"
+        pageTitle: "Trang tổng quan",
+        statistic: statistic
     });
 };
