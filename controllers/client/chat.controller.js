25c4534057493f1af1d48d320134094a0e118a80
muahahammm <aahh853853@gmail.com>
2026-03-31 22:03:29 +0700

    Đưa data ra giao diện chat

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,12 +1,41 @@
+const Chat = require("../../models/chat.model");
+const User = require("../../models/user.model");
+
 // [GET] /chat/
 module.exports.chat = async (req, res) => {
+    const userId = res.locals.user.id;
+
     // Socket.io
-    _io.on('connection', (socket) => {
-        console.log('a user connected', socket.id);
+    _io.once('connection', (socket) => {
+        socket.on('CLIENT_SEND_MESSAGE', async (content) => {
+            // Lưu vào database
+            const chat = new Chat({
+                user_id: userId,
+                content: content
+            });
+            await chat.save();
+        });
     });
     // End socket.io
 
+    // Lấy data từ database
+    const chats = await Chat.find({
+        deleted: false
+    });
+
+    for (const chat of chats) {
+        const infoUser = await User.findOne({
+            _id: chat.user_id,
+        }).select("fullname");
+
+        chat.infoUser = infoUser;
+    }
+
+    console.log(chats);
+    // Kết thúc lấy data từ database
+
     res.render("client/pages/chat/index", {
-        pageTitle: "Chat"
-    })
+        pageTitle: "Chat",
+        chats: chats
+    });
 };
---
+++ public/js/chat.js
@@ -0,0 +1,14 @@
+// SILENT_SEND_MESSAGE 
+const formSendData = document.querySelector(".chat .inner-form");
+if(formSendData) {
+    formSendData.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const content = e.target.elements.content.value;
+        console.log(content);
+        if(content) {
+            socket.emit("CLIENT_SEND_MESSAGE", content);
+            content = "";
+        }
+    });
+}
+// End SILENT_SEND_MESSAGE
